10427e6a503707a7a33eef276b0e99904d91df25
Tillmann Nett <mail@tcrueger.de>
2018-02-27 15:34:17 +0100

    Added date sets

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Validated ratings for German names (long version)
+#'
+#' This dataset contains the individual ratings of all participants
+#' for all of the rated names. See \url{https://osf.io/jepzp/} for
+#' a complete description.
+#'
+#' @format A data frame with 39017 rows and 49 variables:
+#' @encoding UTF-8
+#' @source \url{https://osf.io/jepzp/}
+"names.long"
+
+#' Validated ratings for German names (averaged version)
+#'
+#' This dataset contains the ratings for all 2000 names averaged
+#' across all participants who rated the same name.
+#' See \url{https://osf.io/jepzp/} for a complete description.
+#'
+#' @format A data frame with 2000 rows and 47 variables:
+#' @encoding UTF-8
+#' @source \url{https://osf.io/jepzp/}
+"names.mean"
