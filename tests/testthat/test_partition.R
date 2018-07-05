72a1295f9d0ce39d9a7f8c670676dfa9766da3a4
Tillmann Nett <mail@tcrueger.de>
2018-07-05 15:10:29 +0200

    Added tests for random splits

--- tests/testthat/test_partition.R
+++ tests/testthat/test_partition.R
@@ -20,24 +20,78 @@ context("Partition")
 ###############################################
 
 test_that("Partition have correct sizes", {
+  ##### Setup
+
   n1 <- filter.names()
-  s1 <- partition.names(Sex)
-  expect_equal( length(s1[1]) + length(s1[2]), length(n1))
-
-  s2 <- partition.names(Sex, subset = n1)
-  expect_equal( length(s2[1]) + length(s2[2]), length(n1))
-
   n2 <- filter.names(Competence >= 0.5)
+
+  ##### Attribute Splits
+
+  s1 <- partition.names(Sex)
+  s2 <- partition.names(Sex, subset = n1)
   s3 <- partition.names(Sex, subset = n2)
+
+  # No items are lost or added
+  expect_equal( length(s1[1]) + length(s1[2]), length(n1))
+  expect_equal( length(s2[1]) + length(s2[2]), length(n1))
   expect_equal( length(s3[1]) + length(s3[2]), length(n2))
 
   s4 <- partition.names(Sex, discard=0.2)
-  expect_true( (length(s4[1]) + length(s4[2])) <= length(n1))
-
   s5 <- partition.names(Sex, discard=0.2, subset = n1)
-  expect_true( (length(s5[1]) + length(s5[2])) <= length(n1))
-
   s6 <- partition.names(Sex, discard=0.2, subset = n2)
+
+  # No items are added
+  expect_true( (length(s4[1]) + length(s4[2])) <= length(n1))
+  expect_true( (length(s5[1]) + length(s5[2])) <= length(n1))
   expect_true( (length(s6[1]) + length(s6[2])) <= length(n2))
+
+  #### Random Splits
+
+  s7 <- partition.names.random()
+  s8 <- partition.names.random(subset = n1)
+  s9 <- partition.names.random(subset = n2)
+
+  # all items are kept, none are added
+  expect_equal( length(s7[1]) + length(s7[2]), length(n1))
+  expect_equal( length(s8[1]) + length(s8[2]), length(n1))
+  expect_equal( length(s9[1]) + length(s9[2]), length(n2))
+  # Correct proportions
+  expect_true(abs(length(s7[1]) - length(s7[2])) <= 1)
+  expect_true(abs(length(s8[1]) - length(s8[2])) <= 1)
+  expect_true(abs(length(s9[1]) - length(s9[2])) <= 1)
+
+
+  s10 <- partition.names.random(prop = c(1,2))
+  s11 <- partition.names.random(subset = n1, prop = c(1,2))
+  s12 <- partition.names.random(subset = n2, prop = c(1,2))
+
+  # all items are kept, none are added
+  expect_equal( length(s10[1]) + length(s10[2]), length(n1))
+  expect_equal( length(s11[1]) + length(s11[2]), length(n1))
+  expect_equal( length(s12[1]) + length(s12[2]), length(n2))
+  # Correct proportions
+  expect_true(abs(length(s10[1]) - length(s10[2])/2) <= 1)
+  expect_true(abs(length(s11[1]) - length(s11[2])/2) <= 1)
+  expect_true(abs(length(s12[1]) - length(s12[2])/2) <= 1)
+
+
+  s13 <- partition.names.random(prop = c(1,2,4))
+  s14 <- partition.names.random(subset = n1, prop = c(1,2,4))
+  s15 <- partition.names.random(subset = n2, prop = c(1,2,4))
+
+  # all items are kept, none are added
+  expect_equal( length(s13[1]) + length(s13[2]) + length(s13[3]), length(n1))
+  expect_equal( length(s14[1]) + length(s14[2]) + length(s14[3]), length(n1))
+  expect_equal( length(s15[1]) + length(s15[2]) + length(s15[3]), length(n2))
+  # Correct proportions
+  expect_true(abs(length(s13[1]) - length(s13[2])/2) <= 1)
+  expect_true(abs(length(s14[1]) - length(s14[2])/2) <= 1)
+  expect_true(abs(length(s15[1]) - length(s15[2])/2) <= 1)
+  expect_true(abs(length(s13[1]) - length(s13[3])/4) <= 1)
+  expect_true(abs(length(s14[1]) - length(s14[3])/4) <= 1)
+  expect_true(abs(length(s15[1]) - length(s15[3])/4) <= 1)
+  expect_true(abs(length(s13[2]) - length(s13[3])/2) <= 1)
+  expect_true(abs(length(s14[2]) - length(s14[3])/2) <= 1)
+  expect_true(abs(length(s15[2]) - length(s15[3])/2) <= 1)
 
 })
