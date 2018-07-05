75a8d4387f2072f1aa316fe8a2088339659a9b21
Tillmann Nett <mail@tcrueger.de>
2018-07-05 15:18:55 +0200

    Added tests that splits are disjoint

--- tests/testthat/test_match.R
+++ tests/testthat/test_match.R
@@ -24,31 +24,62 @@ test_that("Matches have correct size", {
   s1 <- filter.names(Familiarity >= 0.5, Nationality >= 0.5)
 
   m1 <- match.pairs(Sex)
-  m2 <- match.pairs(Sex, subset=s0)
-  m3 <- match.pairs(Sex, subset=s1)
-  m4 <- match.pairs(Sex, discard = 0.2)
-  m5 <- match.pairs(Sex, discard = 0.2, subset=s0)
-  m6 <- match.pairs(Sex, discard = 0.2, subset=s1)
-  m7 <- match.pairs(Sex, discard = 0.2, subset=s0, Competence=10, Intelligence=10)
-  m8 <- match.pairs(Sex, discard = 0.2, subset=s1, Competence=10, Intelligence=10)
 
   # We should not have more than original dataset
   expect_true( (length(m1[1]) + length(m1[2])) <= length(s0))
-  expect_true( (length(m2[1]) + length(m2[2])) <= length(s0))
-  expect_true( (length(m3[1]) + length(m3[2])) <= length(s1))
-  expect_true( (length(m4[1]) + length(m4[2])) <= length(s0))
-  expect_true( (length(m5[1]) + length(m5[2])) <= length(s0))
-  expect_true( (length(m6[1]) + length(m6[2])) <= length(s1))
-  expect_true( (length(m7[1]) + length(m7[2])) <= length(s0))
-  expect_true( (length(m8[1]) + length(m8[2])) <= length(s1))
-
-  # Both sets should match in length
+  # Both sets should match in lengt
   expect_equal(length(m1[1]), length(m1[2]))
+
+
+  m2 <- match.pairs(Sex, subset=s0)
+
+  # We should not have more than original dataset
+  expect_true( (length(m2[1]) + length(m2[2])) <= length(s0))
+  # Both sets should match in length
   expect_equal(length(m2[1]), length(m2[2]))
+
+
+  m3 <- match.pairs(Sex, subset=s1)
+
+  # We should not have more than original dataset
+  expect_true( (length(m3[1]) + length(m3[2])) <= length(s1))
+  # Both sets should match in length
   expect_equal(length(m3[1]), length(m3[2]))
+
+
+  m4 <- match.pairs(Sex, discard = 0.2)
+
+  # We should not have more than original dataset
+  expect_true( (length(m4[1]) + length(m4[2])) <= length(s0))
+  # Both sets should match in length
   expect_equal(length(m4[1]), length(m4[2]))
+
+  m5 <- match.pairs(Sex, discard = 0.2, subset=s0)
+
+  # We should not have more than original dataset
+  expect_true( (length(m5[1]) + length(m5[2])) <= length(s0))
+  # Both sets should match in length
   expect_equal(length(m5[1]), length(m5[2]))
+
+  m6 <- match.pairs(Sex, discard = 0.2, subset=s1)
+
+  # We should not have more than original dataset
+  expect_true( (length(m6[1]) + length(m6[2])) <= length(s1))
+  # Both sets should match in length
   expect_equal(length(m6[1]), length(m6[2]))
+
+  m7 <- match.pairs(Sex, discard = 0.2, subset=s0, Competence=10, Intelligence=10)
+
+  # We should not have more than original dataset
+  expect_true( (length(m7[1]) + length(m7[2])) <= length(s0))
+  # Both sets should match in length
   expect_equal(length(m7[1]), length(m7[2]))
+
+
+  m8 <- match.pairs(Sex, discard = 0.2, subset=s1, Competence=10, Intelligence=10)
+
+  # We should not have more than original dataset
+  expect_true( (length(m8[1]) + length(m8[2])) <= length(s1))
+  # Both sets should match in length
   expect_equal(length(m8[1]), length(m8[2]))
 })
